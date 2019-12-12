7048c54b94c93655835cae30e9255e12305b383d
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 13:28:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -45,6 +45,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] = "C"
+     
      return programmer_hash[:dennis_ritchie][:languages]
 end
 
