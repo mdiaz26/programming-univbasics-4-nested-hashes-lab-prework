544729e1a49f34d44ae4c94e1da4b3bc24a45d1a
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 13:46:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,7 +75,9 @@ def adding_matz
      }
 
     programmer_hash[:yukihiro_matsumoto] = 
-      :known_for
+    {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]}
       
       return programmer_hash[:yukihiro_matsumoto]
     
