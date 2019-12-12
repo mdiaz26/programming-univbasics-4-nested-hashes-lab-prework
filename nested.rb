b21c855869bc4bf4649c1a6d6a0e6ae7edf3aa2b
Michael Diaz <mike.diaz006@gmail.com>
2019-12-12 13:22:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,20 +1,9 @@
 
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
+        grace_hopper = {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
-     }
 
 end
 
